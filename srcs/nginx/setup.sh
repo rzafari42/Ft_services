94d6e494d8973b2d822f03cd52ac89ed4ab4467d
rzafari42 <redwane.zafari@hotmail.com>
2020-11-11 13:25:50 +0100

    nginx YAML done, Tests needed

--- srcs/nginx/setup.sh
+++ srcs/nginx/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-service nginx stop
+#service nginx stop
 echo "Welcome"
 docker build -t test .
 echo "startting nginx"
-service nginx start
+#service nginx start
 echo "lets run it"
 docker run -itd -p 443:443 test
 echo "running"
