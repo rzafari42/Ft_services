084f7258124e79fcaeafc20c5b791f04723ed87c
rzafari <rzafari@student.42.fr>
2020-12-18 20:58:56 +0100

    setup.sh added functionalities

--- setup.sh
+++ setup.sh
@@ -43,29 +43,58 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manife
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.4/manifests/metallb.yaml
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-kubectl apply -f ./srcs/metallb/metallb-config.yaml
+kubectl delete -f ./srcs/metallb-conf.yaml
+kubectl apply -f ./srcs/metallb-conf.yaml
 
 IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
 echo "LoadBalancer IP : ${IP}"
+
 
 echo "Let's build every docker images ..."
 echo "nginx ..."
 docker build -t nginx srcs/nginx
 echo "PhpMyAdmin..."
-docker build -t nginx srcs/PhpMyAdmin
+docker build -t PhpMyAdmin srcs/PhpMyAdmin
 echo "Wordpress..."
-docker build -t nginx srcs/wordpress
+docker build -t wordpress srcs/wordpress
 echo "MySQL..."
-docker build -t nginx srcs/MySQL
+docker build -t MySQL srcs/MySQL
 echo "FTPS..."
-docker build -t nginx srcs/ftps
+docker build -t fts srcs/ftps
 echo "Grafana..."
-docker build -t nginx srcs/Grafana
+docker build -t grafana srcs/Grafana
 echo "InfluxDB..."
-docker build -t nginx srcs/InfluxDB
+docker build -t InfluxDB srcs/InfluxDB
 
 
+DB_NAME=wordpress; DB_USER=wp_user; DB_PASSWORD=password; DB_HOST=mysql;
 
+kubectl create secret generic db-id \
+        --from-literal=name=${DB_NAME} \
+        --from-literal=user=${DB_USER} \
+        --from-literal=password=${DB_PASSWORD} \
+        --from-literal=host=${DB_HOST}
+
+kubectl create secret generic rzafari \
+        --from-literal=user="rzafari" \
+        --from-literal=password="password"
+        
+# Service deployment
+echo "About to deploy services ..."
+echo "Let start with nginx ..."
+kubectl create -g ./srcs/nginx-deployment.yaml
+echo "MySQL ..."
+kubectl create -g ./srcs/mysql-deployment.yaml
+echo "wordpress next..."
+kubectl create -g ./srcs/wordpress-deployment.yaml
+echo "then PhpMyAdmin ..."
+kubectl create -g ./srcs/phpmyadmin-deployment.yaml
+echo "and ftps ..."
+kubectl create -g ./srcs/ftps-deployment.yaml
+echo "but also influxdb ..."
+kubectl create -g ./srcs/influxdb-deployment.yaml
+echo "and finally Grafana ..."
+#kubectl create -g ./srcs/grafana-deployment.yaml
 
 # Dachboard
 echo "Dashboard is about to be opened ..."
