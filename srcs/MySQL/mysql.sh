1bcad712fec60c3c1a0a421d907c6bdc643e9bdf
rzafari <rzafari@student.42.fr>
2020-12-31 20:14:54 +0100

    I don't knwo what im doing and i honestly don't care. I'm definitely not made to become a DevOps lol

---
+++ srcs/MySQL/mysql.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+mysql_install_db --user=mysql --datadir=/var/lib/mysql/
+sleep 3
+mysqld --default-authentication-plugin=mysql_native_password &
+sleep 3
+
+tmpsql="/tmp/init_sql"
+echo > $tmpsql \
+"CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+CREATE USER IF NOT EXISTS ${DB_USER} IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+FLUSH PRIVILEGES;
+GRANT ALL ON *.* TO '${DB_USER}'@'127.0.0.1' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;
+GRANT ALL ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}' WITH GRANT OPTION;
+FLUSH PRIVILEGES;"
+
+if [ ! -f /var/lib/mysql/wpNewUsers ]; then
+	echo "done" >> /var/lib/mysql/wpNewUsers
+	mysql -h localhost -e "$(cat $tmpsql)"
+	mysql -h localhost -e "$(cat ./wordpress.sql)"
+	mysql -h localhost -e "$(cat ./new_users.sql)"
+fi
+
+rm -f $tmpsql
+
+/usr/share/mariadb/mysql.server stop
