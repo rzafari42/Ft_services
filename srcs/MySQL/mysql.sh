64f50067289a3a04a552e2565295cea0e5db0516
rzafari <rzafari@student.42.fr>
2021-01-06 16:54:34 +0100

    Mysql is supposed to work now. Let see if I can connect Mysql with Wordpress

--- srcs/MySQL/mysql.sh
+++ srcs/MySQL/mysql.sh
@@ -2,11 +2,13 @@
 
 mkdir -p /run/mysqld && \
 chown -R mysql:mysql /run/mysqld
+mkdir -p ./mysql/
 
 mysql_install_db --user=mysql --datadir=/var/lib/mysql/ #it creates the data directory and build the tables
 
-db_init.sh & #launching the scrip which create the wordpress database and the user (all of that in the background)
+#launching the scrip which create the wordpress database and the user (all of that in the background)
+nohup db_init.sh >/dev/null 2>&1 &
 
 #Starting mysqld. mysqld_safe ensures that the mysqld daemon restart in case of crash
 /usr/bin/mysqld_safe --datadir="/var/lib/mysql/"
- /usr/mysql-test ; perl mysql-test-run.pl
+#cd '/usr/mysql-test' ; perl mysql-test-run.pl
