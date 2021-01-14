67bfcdab50b77ffefef45ff9dfbf69083a220f17
rzafari <rzafari@student.42.fr>
2021-01-14 20:48:01 +0100

    grafana done

--- srcs/Grafana/grafana.sh
+++ srcs/Grafana/grafana.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed s/__GRAFANA_USER__/$GRAFANA_USER/g /usr/share/grafana/conf/grafana.ini -i
-sed s/__GRAFANA_PASSWORD__/$GRAFANA_PASSWORD/g /usr/share/grafana/conf/grafana.ini -i
+sed s/__GRAFANA_USER__/$GRAFANA_USER/g /usr/share/grafana/conf/custom.ini -i
+sed s/__GRAFANA_PASSWORD__/$GRAFANA_PASSWORD/g /usr/share/grafana/conf/custom.ini -i
 
 supervisord
